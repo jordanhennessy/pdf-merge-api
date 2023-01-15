0a1b05fbf40d1fa934d3d30ae5f54475d9fc9aac
jhennessy <hennessyjordan1@gmail.com>
2023-01-15 20:55:03 +0000

    Added basic frontend for merging files

---
+++ frontend/src/FileUploads.js
@@ -0,0 +1,53 @@
+import React, {Component} from "react";
+
+export default class FileUploads extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onFileChange = this.onFileChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            selectedFiles: null
+        }
+    }
+
+    onFileChange(e) {
+        this.setState({selectedFiles: e.target.files})
+    }
+
+    async onSubmit(e) {
+        e.preventDefault();
+
+        let formData = new FormData();
+        for (const key of Object.keys(this.state.selectedFiles)) {
+            formData.append("files", this.state.selectedFiles[key])
+        }
+
+        await fetch("/merge", {
+            method: "POST",
+            body: formData
+        }).then(result => result.blob())
+            .then(blob => {
+                let file = window.URL.createObjectURL(blob);
+                window.location.assign(file)
+            })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <form onSubmit={this.onSubmit}>
+                        <h3>PDF Merger</h3>
+                        <div className="form-group">
+                            <input type="file" name="selectedFiles" onChange={this.onFileChange} multiple/>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Upload</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
