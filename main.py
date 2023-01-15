f8a88046f752e8eb3486b5a74ff7dd70986b4c19
jhennessy <hennessyjordan1@gmail.com>
2023-01-15 15:44:45 +0000

    Added endpoint for merging PDFs

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-from fastapi import FastAPI
+import os
+import shutil
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from PyPDF2 import PdfMerger
+from typing import List
 
 app = FastAPI()
+
+
+@app.get("/status")
+def getStatus():
+    return "OK"
+
+
+@app.post("/merge")
+def merge(files: List[UploadFile] = File(...)):
+    for file in files:
+        with open(f"{file.filename}", "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    files_for_merging = [file.filename for file in files]
+    mergePdfs(files_for_merging)
+    [os.remove(file) for file in files_for_merging]
+    return FileResponse("merged.pdf", filename="merged.pdf")
+
+
+def mergePdfs(pdfs):
+    merger = PdfMerger()
+    [merger.append(pdf) for pdf in pdfs]
+    with open("merged.pdf", "wb") as result:
+        merger.write(result)
 
