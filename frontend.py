37a60ecc92b6a8ec86aa111bb455d07746952963
jhennessy <hennessyjordan1@gmail.com>
2023-01-20 12:28:35 +0000

    Finished frontend

--- frontend.py
+++ frontend.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 import requests as r
+from st_draggable_list import DraggableList
 
 URL = "http://localhost:8000/merge"
 working_dir = os.getcwd()
@@ -9,12 +10,9 @@ working_dir = os.getcwd()
 def send_request(pdfs):
     data = []
     for pdf in pdfs:
-        print(pdf)
         path = working_dir + "/" + pdf
         if os.path.exists(path):
             data.append(("files", open(path, "rb")))
-
-    print(data)
     response = r.post(URL, files=data)
     with open("result.pdf", "wb") as result:
         for chunk in response.iter_content(chunk_size=8192):
@@ -26,7 +24,11 @@ def send_request(pdfs):
 
 
 def callback():
-    send_request(filenames)
+    names = []
+    if len(drag_list) > 0:
+        for element in drag_list:
+            names.append(element["name"])
+    send_request(names)
 
 
 def cleanup():
@@ -36,8 +38,18 @@ def cleanup():
             os.remove(os.path.join(working_dir, entry))
 
 
+def create_draggable_list(names):
+    data = []
+
+    for name in names:
+        data.append({"id": name, "order": len(data) + 1, "name": name})
+
+    return DraggableList(data)
+
+
 st.title("PDF Merger")
-st.write("Files will be merged in the order shown")
+st.write("Files will be merged in the order shown below the merge button")
+st.write("Drag and drop filenames to re-order")
 st.markdown("---")
 
 files = st.file_uploader("Select files for upload", type="pdf", accept_multiple_files=True)
@@ -48,7 +60,4 @@ if len(files) > 0:
             with open(file.name, "wb") as f:
                 f.write(file.getbuffer())
     st.button("Merge", on_click=callback)
-
-
-filenames = [file.name for file in files]
-
+    drag_list = create_draggable_list([file.name for file in files])
