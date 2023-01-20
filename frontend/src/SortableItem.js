97178ffc12c821e0737c505d8142ee3a3b93b94c
jhennessy <hennessyjordan1@gmail.com>
2023-01-20 11:59:41 +0000

    migrated frontend to streamlit

--- frontend/src/SortableItem.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { SortableElement } from "react-sortable-hoc";
-
-const SortableItem = (props) => {
-    return <li>{props.value}</li>
-}
-
-export default SortableElement(SortableItem);
