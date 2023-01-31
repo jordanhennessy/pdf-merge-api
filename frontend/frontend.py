a8e2d603a2d8ef018312a58ca127a3556de5744f
jhennessy <hennessyjordan1@gmail.com>
2023-01-31 18:21:35 +0000

    Fixed file downloading bug

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,4 +1,5 @@
-from os import path, mkdir, remove
+import uuid
+from os import path, mkdir, remove, listdir
 import streamlit as st
 from st_draggable_list import DraggableList
 from utils import merger
@@ -7,16 +8,17 @@ file_dir = "/tmp/files"
 
 
 def callback():
-    filenames = [element["name"] for element in drag_list]
+    filenames = listdir(file_dir)
     result_path = merger.merge_by_name(filenames)
-    cleanup(filenames)
     with open(result_path, "rb") as result:
-        st.download_button("Download merged file", data=result, mime="application/pdf")
+        st.download_button("Download merged file", data=result, mime="application/pdf", on_click=cleanup,
+                           file_name=f"{str(uuid.uuid1())}.pdf")
 
 
-def cleanup(names):
-    for name in names:
+def cleanup():
+    for name in listdir(file_dir):
         remove(f"{file_dir}/{name}")
+    st.session_state["upload"] = False
 
 
 def create_draggable_list(names):
@@ -34,12 +36,15 @@ st.markdown("---")
 files = st.file_uploader("Select files for upload", type="pdf", accept_multiple_files=True)
 
 if len(files) > 0:
-    if not path.exists(file_dir):
-        mkdir(file_dir)
-    for file in files:
-        with open(f"{file_dir}/{file.name}", "wb") as f:
-            f.write(file.getbuffer())
-    st.button("Merge", on_click=callback)
-    drag_list = create_draggable_list([file.name for file in files])
-
-
+    if "upload" not in st.session_state:
+        st.session_state["upload"] = False
+    if st.button("Upload"):
+        st.session_state["upload"] = not st.session_state["upload"]
+    if st.session_state["upload"]:
+        if not path.exists(file_dir):
+            mkdir(file_dir)
+        for file in files:
+            with open(f"{file_dir}/{file.name}", "wb") as f:
+                f.write(file.getbuffer())
+        st.button("Merge", on_click=callback)
+        create_draggable_list([file.name for file in files])
